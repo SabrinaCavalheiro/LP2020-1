4ec7869a94fb45a1f276aa9d898190a5e52f179a
Sabrina Cavalheiro <>
2021-05-17 02:59:05 +0000

    enviando ex.

--- SEMANA09_q17.c
+++ SEMANA09_q17.c
@@ -1,3 +1,69 @@
 // Escreva um programa que leia uma sequência de números inteiros de 0 a 9 e informe a quantidade de ocorrências de cada número. Observe os exemplos para ficar mais claro.
 // Entrada: uma sequência de números inteiros de 0 a 9, seguidos de um número negativo.
 // Saídas: a lista de números de 0 a 9 e o número de ocorrências separados pelo caractere dois pontos (:), cada um em uma linha, em ordem, desde que o número tenha aparecido ao menos uma vez.
+
+
+
+// #include<stdio.h>
+
+// int main(void){
+	
+// 	int a,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0;
+	
+// 	while(a>=0){
+// 		scanf("%i", &a);
+// 		if(a==0){
+// 			b++;
+// 		}else if(a==1){
+//       c++;
+// 		}
+//     else if(a==2){
+// 			d++;
+// 		}
+//     else if(a==3){
+// 			e++;
+// 		}
+//     else if(a==4){
+// 			f++;
+// 		}
+//     else if(a==5){
+// 			i++;
+// 		}
+//     else if(a==6){
+// 			h++;
+// 		}
+//     else if(a==7){
+// 			i++;
+// 		}
+//     else if(a==8){
+// 			j++;
+// 		}
+//     else if(a==9){
+// 			k++;
+// 		}
+// 	}if(b>=1){
+// 		printf("0:%i\n",b);
+//   }if(c>=1){
+// 		printf("1:%i\n",c);	
+// 	}if(d>=1){
+// 		printf("2:%i\n",d);
+// 	}if(e>=1){
+// 		printf("3:%i\n",e);	
+// 	}if(f>=1){
+// 		printf("4:%i\n",f);	
+// 	}if(g>=1){
+// 		printf("5:%i\n",g);
+// 	}if(h>=1){
+// 		printf("6:%i\n",h);	
+// 	}if(i>=1){
+// 		printf("7:%i\n",i);	
+// 	}if(j>=1){
+// 		printf("8:%i\n",j);
+// 	}if(k>=1){
+// 		printf("9:%i\n",k);
+// 	}
+	
+// 	return 0;
+// }
+
+
