f4420a0099347452a1823a3071d2757ededea656
Sabrina Cavalheiro <>
2021-05-19 21:00:03 +0000

    enviando ex.

--- SEMANA09_q15.c
+++ SEMANA09_q15.c
@@ -1,12 +1,11 @@
 // Imagine que em uma fábrica qualquer exista uma caldeira onde fluidos de alta temperatura são armazenados. Nela está acoplado um sensor que mede sua temperatura periodicamente. Escreva um programa que leia uma sequência de valores de temperatura lidos pelo sensor até que um valor negativo seja fornecido. O programa deve informar a mensagem “DEU RUIM” se em algum momento um valor de temperatura lido for maior que 1000º C, indicando uma condição perigosa de funcionamento, ou deve informar “TURNO TRANQUILO” se ao final da leitura da sequência nenhum valor lido foi maior que 1000. Entrada: Uma sequência de números inteiros seguido por um valor negativo. Saída: DEU RUIM ou TURNO TRANQUILO, conforme as regras especificadas no enunciado.
 
   
-  
 // #include<stdio.h>
 // int main(void){
 //   int a=-1,b=0;
 //   do{
-//   scanf("%i/n", &a);
+//   scanf("%i\n", &a);
 //   if  (a>1000){
 //     b=a;
 //   }
@@ -21,5 +20,9 @@
   
 // 	return 0;
 // }
+
+
+  
+
 
 
--- SEMANA09_q20.c
+++ SEMANA09_q20.c
@@ -4,3 +4,42 @@
 // Saídas: Uma lista de 10 números inteiros em ordem crescentes escritos em uma linha e
 // separados por espaço, seguido de uma segunda linha com os mesmos números escritos em
 // ordem decrescente separados por espaço
+
+
+// #include <stdio.h>
+
+// int main(void){
+//   int a,b,c,v[10];
+
+//   for (a=0;a<10;a++){
+//     scanf("%i",&v[a]);
+//   }
+//   for(a=0;a<10;a++){
+// 		for(b=a+1;b<10;b++){
+// 			if(v[a]>v[b]){
+// 			c=v[a];
+// 			v[a]=v[b];
+// 			v[b]=c;
+// 			}
+// 		}
+// 	}
+// 	for(a=0;a<10;a++){
+// 		printf("%i ",v[a]);
+// 	}
+// 	printf("\n");
+// 	for(a=0;a<10;a++){
+// 		for(b=a+1;b<10;b++){
+// 			if(v[a]<v[b]){
+// 			c=v[a];
+// 			v[a]=v[b];
+// 			v[b]=c;
+// 			}
+// 		}
+// 	}
+// 	for(a=0;a<10;a++){
+// 		printf("%i ",v[a]);
+// 	}
+
+// return 0;
+// }
+
